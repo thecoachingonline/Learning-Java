aea125958700a889a2a11f6e2c2cae8b01714d97
Y.Nanthachai <thecoachingonline@gmail.com>
2024-01-22 10:11:32 +0000

    Data types and variables

---
+++ BuildingStrongFoundations/DataAndVariables.java
@@ -0,0 +1,17 @@
+public class DataAndVariables {
+
+    public static void main(String[] args) {
+
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'N';
+        char studentLastInitial = 'Y';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+    }
+}
