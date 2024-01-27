03c51860116150610010d1df34935196cf1d4ac8
Y.Nanthachai <thecoachingonline@gmail.com>
2024-01-27 05:26:37 +0000

    Concatenating

---
+++ BuildingStrongFoundations/Concatenating.java
@@ -0,0 +1,10 @@
+public class Concatenating {
+
+    public static void main(String[] args) {
+        double studentGPA = 3.45;
+        String studentFirstName = "Nanthachai";
+        String studentLastName = "Yuennan";
+
+        System.out.println(studentFirstName + " " + studentLastName + " คะแนน GPA " + studentGPA);
+    }
+}
