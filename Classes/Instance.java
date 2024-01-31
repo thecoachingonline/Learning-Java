08addb0d5eaa51c89beac287b8b0312ec8115029
Y.Nanthachai <thecoachingonline@gmail.com>
2024-01-31 08:30:54 +0000

    Creating an instance

---
+++ Classes/Instance.java
@@ -0,0 +1,19 @@
+public class Instance {
+    double base;
+    double height;
+    double sideLenOne;
+    double sideLenTwo;
+    double sideLenThree;
+    
+    public Instance(double base, double height, double sideLenOne, double sideLenTwo, double sideLenThree) {
+        this.base = base;
+        this.height = height;
+        this.sideLenOne = sideLenOne;
+        this.sideLenTwo = sideLenTwo;
+        this.sideLenThree = sideLenThree;
+    }
+
+    public double findArea() {
+        return (this.base * this.height) / 2;
+    }
+}
---
+++ Classes/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        Instance instanceA = new Instance(15, 8, 15, 8, 17);
+        Instance instanceB = new Instance(3, 2.598, 3, 3, 3);
+    }
+}
